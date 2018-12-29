c89365b969ec2d12f4e3b26e2f1765a57b9826a8
notmiro <45168643+notmiro@users.noreply.github.com>
2018-12-29 14:28:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,97 @@
-const Eris = require("eris");
-var x5bzteam = new Eris("MzM3Mzk4ODQzNDU4OTc3Nzky.DwDKhQ.xJW3iixRVelFsq-0cG6GGbMHQ4M");
-x5bzteam.on("ready", () => {
-    console.log("Yo! Changer [ON]");
-    onstart();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready' , ready => {
+///
+console.log(`Done`)
+/// 
 });
-x5bz = "433016026561773608";
-nick = "! NotMiro";
-function onstart(){
-    var v1 = nick.split('');
-    var counter;
-    var i=0;
-var x5bz2 = ' ';
-  var x5bz3 =   setInterval(function(){
-     
-     if (i == v1.length){
-     clearInterval(x5bz3);
-     onstart()
-      return;
-     }
-     var v2 = v1[i];
-    x5bz2 += v2;
-    x5bzteam.editNickname(x5bz, x5bz2);
+
+client.on('ready', ready => {
+client.user.setGame(`Galaxy Server`,`https://twitch.tv/ichbinxirdx`)
+});
+
+var prefix = "G"
+client.on("message", (message) => {
+    if (message.content.startsWith("Gkick")) {
+      if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply('? ماعندك الصلاحيات');
+        var member= message.mentions.members.first();
+        member.kick().then((member) => {
+            message.channel.send(member.displayName + " مع السلامه :wave: ");
+        }).catch(() => {
+            message.channel.send("Error -_-");
+        });
+    }
+});
+
+var prefix = "G"
+client.on('message', message => {
+  if (message.author.xErenaa) return;
+  if (!message.content.startsWith(prefix)) return;
  
-        i++;
-   
-     }, 1000);
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let bErenaalog = client.channels.find("name", "Erenaa-log");
+ 
+  if(!bErenaalog) return message.reply("I've detected that this server doesn't have a Erenaa-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+ 
+  message.guild.member(user).ban(7, user);
+ 
+  const banembed = new Discord.RichEmbed()
+  .setAuthor(`BANNED!`, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : banembed
+  })
 }
-x5bzteam.connect();
+});
+
+client.on('message', message => {
+    var prefix = "G";
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**Gbc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    }); 
+	
+	
+client.login(process.env.BOT_TOKEN);
